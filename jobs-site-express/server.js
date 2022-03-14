be4a02db3979d8bba564e8b1fe8c87a766259208
cncrx <cngzcrx@163.com>
2022-03-14 15:51:24 +0800

    express: added express-rate-limit

--- jobs-site-express/server.js
+++ jobs-site-express/server.js
@@ -4,11 +4,18 @@ const parser = require("body-parser")
 const morgan = require("morgan")
 const cors = require("cors")
 
+const RateLimiter = require("express-rate-limit")
+const limiter = RateLimiter({
+  windowMs: 1*60*1000,
+  max: 10
+})
+
 const app = express()
 app.use(cors())
 app.use(parser.json())
 app.use(parser.urlencoded({ extended: true }))
 app.use(morgan("combined"))
+app.use(limiter)
 
 app.listen(3000, () => console.log("=== server started ==="))
 
